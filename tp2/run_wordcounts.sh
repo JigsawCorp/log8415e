954d54601a9bbf5cba974ae064d9f475ba56b32b
Philippe Cote-Morneault <philippe.cote.11@gmail.com>
2020-11-14 16:02:50 -0500

    Fixed bad directory when calling run_wordcounts

--- tp2/run_wordcounts.sh
+++ tp2/run_wordcounts.sh
@@ -2,9 +2,9 @@
 
 echo "Started running all WordCount methods"
 
-source ./wordcount_linux.sh
-source ./wordcount_hadoop.sh
-source ./wordcount_spark_hdfs.sh
-source ./wordcount_spark_s3.sh
+source log8415e/tp2/wordcount_linux.sh
+source log8415e/tp2/wordcount_hadoop.sh
+source log8415e/tp2/wordcount_spark_hdfs.sh
+source log8415e/tp2/wordcount_spark_s3.sh
 
 echo "Finished running all WordCount methods"
