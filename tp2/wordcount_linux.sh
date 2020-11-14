3f60ed162c2c0b63636b72e24b99cc077a30f2d0
Philippe Cote-Morneault <philippe.cote.11@gmail.com>
2020-11-14 12:21:30 -0500

    Improved linux command output

--- tp2/wordcount_linux.sh
+++ tp2/wordcount_linux.sh
@@ -4,10 +4,10 @@
 echo "Executing Linux command"
 for i in {1..5}
 do
-	echo "data$i:" >> results/linux_time.txt
+	echo "Executing WordCount for data$i:" >> results/linux_time.txt
 	for j in {1..5}
 	do
-		echo "Execution $j:" >> results/linux_time.txt
+		echo "data$i - Execution $j:" >> results/linux_time.txt
 		(time cat input_datasets/data$i.txt | tr ' ' '
 		' | sort | uniq -c > results/linux_data$i.txt) &>> results/linux_time.txt 	
 	done	
