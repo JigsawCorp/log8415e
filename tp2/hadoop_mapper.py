9595636309aefa971f8e1ff83af4b3d0f3d667df
Philippe Cote-Morneault <philippe.cote.11@gmail.com>
2020-11-16 14:08:48 -0500

    Added comments in hadoop map reduce.

--- tp2/hadoop_mapper.py
+++ tp2/hadoop_mapper.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3 
+
+# Code inspired by https://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-python/ 
+# and https://levelup.gitconnected.com/map-reduce-with-python-hadoop-on-aws-emr-341bdd07b804
+
 import sys 
 import string
 try: 
     for line in sys.stdin: 
+        # Remove trailing whitespace and split each word in a list
         line = line.strip() 
         words = line.split(' ') 
         for w in words: 
-            #table = w.maketrans('', '', string.punctuation)
-            #w = w.translate(table).lower()
+            # Only print actual words
             if w: 
                 print(w, '\t', 1)
 except:
